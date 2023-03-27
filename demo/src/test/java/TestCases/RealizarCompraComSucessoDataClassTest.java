6670fe4046171e94514127a27327db36d3f3e730
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-03-27 15:07:39 -0300

    Add files via upload

---
+++ demo/src/test/java/TestCases/RealizarCompraComSucessoDataClassTest.java
@@ -0,0 +1,35 @@
+package TestCases;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+
+import Framework.TestBase;
+import Tasks.CheckoutTask;
+import Tasks.FinishTask;
+import Tasks.LoginTask;
+import Tasks.ProdutoTask;
+
+public class RealizarCompraComSucessoDataClassTest extends TestBase {
+     
+    private WebDriver driver = getDriverManager();
+LoginTask loginTask = new LoginTask(driver);
+ProdutoTask produtoTask = new ProdutoTask(driver);
+CheckoutTask checkoutTask = new CheckoutTask(driver);
+FinishTask finishTask = new FinishTask(driver);
+
+@ParameterizedTest  
+@MethodSource("Framework.Utils.DataClass#loginTestData")
+public void realizarCompra(String user, String password) {
+    
+    loginTask.efetuarLoginParametrizado(user, password);
+    produtoTask.selecionarProduto();
+    checkoutTask.preencherForm();
+    finishTask.finalizarCompra();
+    
+    
+}
+
+}
